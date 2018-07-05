d4c7044c6a14099a58bbcb130cfb125033cedd3f
Jiwenguan <1013233457@qq.com>
2018-07-06 07:49:07 +0800

    ftp已实现

--- 02ftp/c/client.cpp
+++ 02ftp/c/client.cpp
@@ -9,12 +9,15 @@
 #include<stdlib.h>
 #include<sys/types.h>          /* See NOTES */
 #include<sys/socket.h>
+#include<sys/stat.h>
+#include<fcntl.h>
 #include<unistd.h>
 #include<string.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<fstream>
 #include<string.h>
+#include<string>
 using namespace std;
 typedef sockaddr_in SA4;
 typedef sockaddr SA;
@@ -27,7 +30,7 @@ int main(int argc,char *argv[]){
 	//prepare network address
 	SA4 addrC;
 	addrC.sin_family=AF_INET;
-	addrC.sin_port=8080;
+	addrC.sin_port=8000;
 	inet_pton(AF_INET,"127.0.0.1",&(addrC.sin_addr.s_addr));
 	//connect
 	int c = connect(fd,(SA*)&addrC,sizeof(addrC));
@@ -37,25 +40,63 @@ int main(int argc,char *argv[]){
 	char buff[1024]={0};//cin>>;
 	char f[1024]={0};
 	while(1){
-		scanf("%s",buff);
+		cout<<"请输入要下载到本地的文件路径,以回车结束,输exit 退出"<<endl;
+		memset(buff,0,1024);
+		memset(f,0,1024);
+		cin>>buff;
 		if(!strncmp(buff,"exit",4)){
+			cout<<"退出传输。。。"<<endl;
 			break;
 		}	
 		write(fd,buff,strlen(buff));
-		
-		read(fd,f,1023);
-		if(strncmp(f,"file",4)){
-			break;
+		int m=read(fd,f,20);
+		cout<<f<<endl;
+		f[m]='\0';		
+		if(strncmp(f,"file",4)){//如果没有这个文件
+			cout<<"远程没有该文件"<<endl;
+			continue;
+		}
+		//将文件路径中名字提取出来,
+		string tmp(buff);
+		size_t pos = tmp.rfind('/');
+		string store;
+		if(pos!=-1){
+			store = tmp.substr(pos+1);
+		}else{
+			store=tmp;
 		}
 		
-		ofstream ofs(buff,ios::out);
+		cout<<"您的文件将存储在 "<<store<<" 中"<<endl;
+				
+		size_t ffd=open(store.c_str(),O_RDWR|O_CREAT|O_TRUNC,S_IRWXU);
+		if(ffd==-1){
+			perror("open");
+		}	
 		while(1){
-			int r =read(fd,f,1023);
-			if(r<=0)break;
-			ofs.write(f,1023);
+			memset(f,0,1024);
+			int r=read(fd,f,sizeof(f)-sizeof(f[0]));
+			if(r <1023){
+				write(ffd,f,r);
+				cout<<"读取结束。。。"<<endl;
+				break;
+			}
+			write(ffd,f,r);
 		}
+
+
+
+
+		/*ofstream ofs(store.c_str(),ios::out);
+		while(1){
+			int r =read(fd,f,sizeof(f)-sizeof(f[0]));
+			f[r]='\0';
+			cout<<f<<"1"<<endl;
+			if(r<=0)break;
+			ofs.write(f,r);
+		}*/
 		cout<<"文件传输完成"<<endl;
-		ofs.close();
+		close(ffd);
+		//ofs.close();
 		
 	}
 	close(fd);
--- 02ftp/server.cpp
+++ 02ftp/server.cpp
@@ -6,6 +6,8 @@
 //*******************************************************
 #include<stdio.h>
 #include<iostream>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<errno.h>
@@ -15,13 +17,23 @@
 #include<sys/types.h> 
 #include<sys/socket.h>
 #include<fstream>
+#include <signal.h>
+#include<string.h>
 using namespace std;
+//用户自定义的信号处理函数
+void doit(int n){
+    close(sockfd);
+    return;
+}
+int sockfd;
 typedef sockaddr_in SA4;
 typedef sockaddr SA;
 int main(int argc,char* argv[]){
+	signal(2,doit);//注册用户自定义的信号处理函数
+	signal(3,doit);//注册用户自定义的信号处理函数
 	//create socket descriptor
 	
-	int sockfd=socket(AF_INET,SOCK_STREAM,0);//
+	sockfd=socket(AF_INET,SOCK_STREAM,0);//
 	
 	if(sockfd==-1){
 		//print failed message;
@@ -32,7 +44,7 @@ int main(int argc,char* argv[]){
 	//prepare address
 	SA4 addrS;
 	addrS.sin_family=AF_INET;
-	addrS.sin_port=8080;
+	addrS.sin_port=8000;
 	inet_pton(AF_INET,"127.0.0.1",&(addrS.sin_addr.s_addr));
 	//bind sockfd with address
 	if(bind(sockfd,(SA*)&addrS,sizeof(addrS))==-1){
@@ -64,32 +76,65 @@ int main(int argc,char* argv[]){
 		if(pid==0){//child process
 			//cout<<"child process..."<<endl;
 			close(sockfd);
-			char buf[1024]={0};
+			char buf[1024]={0};//收客户端请求的
 			cout<<"client ip: "<<ip<<endl;
+			while(1){
 			//接收客户端发来的文件请求，文件名称不超过1023个字符
 			ssize_t rlen = recv (cfd, buf,sizeof (buf) - sizeof (buf[0]), 0);
 			buf[rlen]='\0';
+			cout<<"recv  client message..."<<buf<<endl;
 			//打开该文件读取其中数据。
-			ifstream ifs(buf,ios::in|ios::binary);
+			size_t ffd=open(buf,O_RDONLY);
+			if(ffd==-1){
+				perror("open");
+				char f[]="no such file";
+				send(cfd,f,strlen(f),0);
+				continue;
+				
+			}	
+			cout<<"start transmission..."<<endl;
+			char f[]="file transmission...";
+			send(cfd,f,strlen(f),0);
+			cout<<"send client success message..."<<f<<endl;
+			while(1){
+				memset(buf,0,1024);
+				int r =read(ffd,buf,1024);
+				if(r<=0)break;
+				send(cfd,buf,r,0);
+			
+			}			
+			close(ffd);
+			cout<<"结束传输"<<endl;
+			}
+			
+
+
+
+
+			/*ifstream ifs(buf,ios::in|ios::binary);
 			if(ifs==0){
 				cout<<"open failed..."<<endl;
 				char f[]="no such file";
 				send(cfd,f,strlen(f),0);
 				return -1;
 			}
+			cout<<"start transmission..."<<endl;
 			char f[]="file transmission...";
 			send(cfd,f,strlen(f),0);
+			cout<<"send client success message..."<<f<<endl;
 			char p[1024]={0};
-			while(1){
-				//读取1023个字节数据到p中
-				ifs.read(p,1023);
-				send(cfd,p,1023,0);//向文件描述符发送数据
-				if(ifs.eof()){
-					cout<<"文件传输完毕"<<endl;
-					ifs.close();
+				while(1){
+					//读取1023个字节数据到p中
+					ifs.read(p,1);
+					if(ifs.eof()){
+						cout<<"文件传输完毕"<<endl;
+						ifs.close();
+						break;
+					}
+					send(cfd,p,1,0);//向文件描述符发送数据
 				}
-			
-			}
+				*/
+			cout<<"close cfd..."<<endl;
 			close(cfd);
 			exit(0);
 		}
